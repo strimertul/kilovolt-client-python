c84c627089fedded70f5f9c4e7ffb063606998eb
Ash Keel <ash@nebula.cafe>
2022-12-22 02:04:48 +0100

    lib setup done

--- example/main.py
+++ example/main.py
@@ -4,11 +4,11 @@ import asyncio
 import os
 from kilovolt.connection import KilovoltClient
 
+
 async def connect():
     kv = KilovoltClient("ws://localhost:4337/ws", os.getenv("KILOVOLT_PASSWORD"))
     await kv.connect()
     await kv.subscribe_prefix("twitch", lambda key, val: print(val))
     print(await kv.list("twitch"))
-    await asyncio.sleep(100)
 
 asyncio.run(connect())
